060f11fb758a457b9bb875739f3fde19bccc2a7c
Kolyan Rayman <chechnev.2001@mail.ru>
2021-06-02 17:59:18 +0300

    Добавил два класса Cell и State, ну и чутка изменил Checkers

---
+++ Сheckers/Сheckers/Cell.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "State.h"
+#include <utility>
+
+using namespace std;
+
+
+/*
+Класс клетка (Cell), для работы с шашками
+*/
+class Cell
+{
+public:
+	
+	/*
+	Конструктор и деструктор
+	*/
+	Cell(const pair <size_t, size_t>& cPosition, State cstate);
+	~Cell();
+
+	/*
+	геттеры
+	*/
+	State GetState() const; 
+	const pair <size_t, size_t>& GetPosition() const;
+
+	/*
+	сеттеры
+	*/
+	void SetState(State cState); 
+	void SetPosition(const pair <size_t, size_t>& cPosition);
+
+
+private:
+	State cState;
+	pair <size_t, size_t> cPosition; //шаблон для координат шашек на поле
+	
+};
+
---
+++ Сheckers/Сheckers/State.h
@@ -0,0 +1,11 @@
+#pragma once
+/*
+Класс перечисления (enum), для отдельных примитивных типов данных
+*/
+enum class State
+{
+	BLACK, 
+	WHITE, 
+	BLANK
+};
+
